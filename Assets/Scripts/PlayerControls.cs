c09f36e3e58bbe023d0659ee8ba20cea2297e4a9
allen <allen@PC-POE>
2026-06-20 20:46:43 -0400

    Add project files.

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 input = Vector2.zero;
+
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.aKey.isPressed)
+                input.x -= 1;
+
+            if (Keyboard.current.dKey.isPressed)
+                input.x += 1;
+
+            if (Keyboard.current.sKey.isPressed)
+                input.y -= 1;
+
+            if (Keyboard.current.wKey.isPressed)
+                input.y += 1;
+        }
+
+        if (Gamepad.current != null)
+        {
+            input += Gamepad.current.leftStick.ReadValue();
+        }
+
+        input = Vector2.ClampMagnitude(input, 1f);
+
+        Vector3 movement = new Vector3(input.x, 0f, input.y);
+
+        rb.AddForce(movement * speed);
+    }
+}
